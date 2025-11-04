318ee3fcf624ae6a330aaa827da11dea44c8c1e9
HaikuKing <haikuking6301@gmail.com>
2025-11-04 16:43:04 -0600

    basic structure for TextNode, HTMLNode, and LeafNode

---
+++ src/htmlnode.py
@@ -0,0 +1,36 @@
+class HTMLNode:
+    def __init__(self, tag=None, value=None, children=None, props=None):
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.props = props
+
+    def to_html(self):
+        raise NotImplementedError("to_html method not implemented")
+    
+    def props_to_html(self):
+        if self.props is None:
+            return ""
+        result = ""
+        for prop in self.props:
+            result += f' {prop}="{self.props[prop]}"'
+        return result
+
+    def __repr__(self):
+        return f"HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})"
+
+class LeafNode(HTMLNode):
+
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value)
+        self.props = props
+    
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("leaf nodes must have a value")
+        elif self.tag is None:
+            return f"{self.value}"
+        elif self.props is None:
+            return f"<{self.tag}>{self.value}</{self.tag}>"
+        else:
+            return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
