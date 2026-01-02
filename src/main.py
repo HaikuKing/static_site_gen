59601a4a2b6ed4998a9c4678e146a3b9d928eb0d
HaikuKing <haikuking6301@gmail.com>
2026-01-01 22:54:48 -0600

    still testing

--- src/main.py
+++ src/main.py
@@ -5,13 +5,14 @@ from constants import basepath, DOCS, STATIC, CONTENT, TEMPLATE
 
 abs_static = os.path.abspath(os.path.join("./", STATIC))
 abs_docs = os.path.abspath(os.path.join("./", DOCS))
+base_docs = os.path.abspath(os.path.join(abs_docs, basepath.strip('/')))
 abs_content = os.path.abspath(os.path.join("./", CONTENT))
 abs_template = os.path.abspath(os.path.join("./", TEMPLATE))
 
 def main():
 
     remove_code(abs_docs)
-    copy_code(abs_static, abs_docs)
+    copy_code(abs_static, base_docs)
     generate_pages_recursive(abs_content, abs_template, abs_docs, basepath)
     
 
